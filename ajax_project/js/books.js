8d77b946e0f6a5a6fccd15a2a492982949305397
Yuval <yuvalmaderer12@gmail.com>
2024-05-26 14:17:54 +0300

    changes

--- ajax_project/js/books.js
+++ ajax_project/js/books.js
@@ -1,4 +1,29 @@
+const url = 'http://localhost:8001/books'
+const bookProperties = document.getElementById("popupBook")
 
-function updateNumOfCopies (id) {
-        
+document.getElementById('searchBookByStr').addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const input = document.getElementById('searchInput').value;
+    const displayBooksBySearch = document.getElementById('displayBooksBySearch');
+    const spinner = document.querySelector(".spinner");
+    displayBooksBySearch.innerHTML = ''
+    const response = await axios.get(url);
+    console.log(response.data);
+    for (const book of response.data) {
+        if (book.title.includes(input)) {
+            displayBooksBySearch.innerHTML += 
+            `
+            <div class="bookCard" onclick="openBookProperties()">
+                <img src=${book.img}
+                <h2>${book.title}</h2>
+                <p>Author: ${book.author}</p>
+                <p>Author: ${book.description}</p>
+            </div>
+            `;
+        }
+    }
+})
+
+function openBookProperties() {
+    bookProperties.style.display = 'block';
 }
