5ccf2bdd4504d69defdcd20957a73e85e2252530
Yuval <yuvalmaderer12@gmail.com>
2024-05-26 14:57:14 +0300

    asddf

--- ajax_project/js/books.js
+++ ajax_project/js/books.js
@@ -1,28 +1,27 @@
-const url = 'http://localhost:8001/books'
-const bookProperties = document.getElementById("popupBook")
+const url = 'http://localhost:8001/books';
+const bookProperties = document.getElementById("popupBook");
 
 document.getElementById('searchBookByStr').addEventListener('submit', async function(e) {
     e.preventDefault();
-    const input = document.getElementById('searchInput').value;
+    const input = document.getElementById('searchInput').value.trim();
     const displayBooksBySearch = document.getElementById('displayBooksBySearch');
-    const spinner = document.querySelector(".spinner");
-    displayBooksBySearch.innerHTML = ''
+    displayBooksBySearch.innerHTML = '';
     const response = await axios.get(url);
-    console.log(response.data);
+
     for (const book of response.data) {
-        if (book.title.includes(input)) {
+        if (book.title.toLowerCase().includes(input.toLowerCase())) {
             displayBooksBySearch.innerHTML += 
             `
             <div class="bookCard" onclick="openBookProperties()">
-                <img src=${book.img}
+                <img src="${book.img}" alt="${book.title}">
                 <h2>${book.title}</h2>
                 <p>Author: ${book.author}</p>
-                <p>Author: ${book.description}</p>
+                <p>Description: ${book.description}</p>
             </div>
             `;
         }
     }
-})
+});
 
 function openBookProperties() {
     bookProperties.style.display = 'block';
